0fa4e4a48ed375f334544b56376f0c58105eeefb
Dj Walker-Morgan <dj@Joi.local>
2019-09-07 08:39:36 +0100

    Tweaks

--- faller.go
+++ faller.go
@@ -1,68 +1,116 @@
 package main
 
+// TODO: Sweep for ammo
+// TODO: Explode shapes on bottom
+// TODO: Better shape templats
+// TODO: Fast bullet/shape/particle collision tests
+
 import (
+	"fmt"
+	"image/color"
 	"log"
 	"math/rand"
 
+	"github.com/golang/freetype/truetype"
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/text"
+	"golang.org/x/image/font"
 )
 
 // World represents the game state.
 type World struct {
-	area     []bool
-	width    int
-	height   int
-	shipxpos int
-	shipload int
-	dropping []Shape
+	width     int
+	height    int
+	base      Base
+	dropbear  int
+	dropstart int
+	dropping  []Shape
+	bullets   []Bullet
 }
 
+// Shape represents a shape
 type Shape struct {
 	xpos    int
 	ypos    int
 	tettype int
+	bmap    [][]bool
+	offxpos int
+	offypos int
 }
 
-func (s *Shape) draw(area []bool, width int, heigh int) {
-	area[s.ypos*width+s.xpos] = true
+func (s *Shape) initial(xpos int, ypos int) {
+	s.tettype = 0
+	s.xpos = xpos
+	s.ypos = ypos
+	switch s.tettype {
+	case 0:
+		s.bmap = make([][]bool, 3)
+		s.bmap[0] = []bool{false, true, false, true, false}
+		s.bmap[1] = []bool{false, true, true, true, false}
+		s.bmap[2] = []bool{false, false, true, false, false}
+		s.offxpos = 2
+		s.offypos = 1
+	}
 }
 
+var (
+	smallArcadeFont font.Face
+)
+
+// Particle is a debris
 type Particle struct {
 	xpos int
 	ypos int
 }
 
+// Bullet is bullet :)
+type Bullet struct {
+	xpos int
+	ypos int
+}
+
+// Base represents our base
+type Base struct {
+	xpos    int
+	ypos    int
+	load    int
+	maxload int
+}
+
+const smallFontSize = 8
+
 // NewWorld clears a world
 func NewWorld(width, height int) *World {
 	w := &World{
-		area:     make([]bool, width*height),
-		width:    width,
-		height:   height,
-		shipxpos: width / 2,
+		width:     width,
+		height:    height,
+		dropbear:  100,
+		dropstart: 100,
+		base:      Base{xpos: width / 2, ypos: height - 1, load: 20 / 2, maxload: 20},
 	}
 
 	return w
 }
 
 func (w *World) init() {
-	for y := 0; y < w.height; y++ {
-		for x := 0; x < w.width; x++ {
-			w.area[y*w.width+x] = false
-		}
-	}
 	w.dropping = make([]Shape, 20)
+	w.bullets = make([]Bullet, 20)
 }
 
 // Update game state by one tick.
-func (w *World) Update() {
+func (w *World) logicupdate() {
 	width := w.width
 	height := w.height
 
-	// This is the screen we will draw on
-	next := make([]bool, width*height)
-
 	// Add a shape
-	w.dropping = append(w.dropping, Shape{ypos: 0, xpos: rand.Intn(width)})
+	w.dropbear = w.dropbear - 1
+	if w.dropbear < 0 {
+		newshape := &Shape{}
+		newshape.initial(0, rand.Intn(width))
+		w.dropping = append(w.dropping, *newshape)
+		w.dropbear = w.dropstart
+	}
 
 	// Move all the shapes
 
@@ -74,75 +122,97 @@ func (w *World) Update() {
 		}
 	}
 
-	// Draw all the shapes
-
-	for _, d := range w.dropping {
-		d.draw(next, width, height)
+	for i, b := range w.bullets {
+		if b.ypos > 0 {
+			w.bullets[i].ypos = b.ypos - 1
+		} else {
+			// Through the roof
+		}
 	}
-
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		if w.shipxpos > 10 {
-			w.shipxpos = w.shipxpos - 1
+		if w.base.xpos > 10 {
+			w.base.xpos = w.base.xpos - 1
 		}
 	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		if w.shipxpos < (w.width - 10) {
-			w.shipxpos = w.shipxpos + 1
+		if w.base.xpos < (w.width - 10) {
+			w.base.xpos = w.base.xpos + 1
+		}
+	} else if ebiten.IsKeyPressed(ebiten.KeyZ) {
+		if w.base.load > 0 {
+			w.bullets = append(w.bullets, Bullet{ypos: w.base.ypos - 1, xpos: w.base.xpos})
+			w.base.load = w.base.load - 1
 		}
 	}
-	next[(height-1)*width+w.shipxpos] = true
-	next[(height-1)*width+(w.shipxpos-1)] = true
-	next[(height-1)*width+(w.shipxpos+1)] = true
-	next[(height-2)*width+w.shipxpos] = true
 
-	w.area = next
 }
 
-func pixelDrop(w *World, x, y int) bool {
-	if w.area[y*w.width+x] {
-		return true
-	}
-	return false
-}
-
-func (w *World) Draw(pix []byte) {
-	for i, v := range w.area {
-		if v {
-			pix[4*i] = 0xff
-			pix[4*i+1] = 0xff
-			pix[4*i+2] = 0xff
-			pix[4*i+3] = 0xff
-		} else {
-			pix[4*i] = 0
-			pix[4*i+1] = 0
-			pix[4*i+2] = 0
-			pix[4*i+3] = 0
+func (s *Shape) draw(screen *ebiten.Image) {
+	for iy, c := range s.bmap {
+		for ix, p := range c {
+			if p {
+				screen.Set(s.xpos+ix, s.ypos+iy, color.White)
+			}
 		}
 	}
+}
+
+func (b *Bullet) draw(screen *ebiten.Image) {
+	screen.Set(b.xpos, b.ypos, color.White)
+}
+
+func (b *Base) draw(screen *ebiten.Image) {
+	screen.Set(b.xpos-1, b.ypos, color.White)
+	screen.Set(b.xpos, b.ypos, color.White)
+	screen.Set(b.xpos+1, b.ypos, color.White)
+	screen.Set(b.xpos, b.ypos-1, color.White)
+}
+
+// Draw the world
+func (w *World) Draw(screen *ebiten.Image) {
+	for _, d := range w.dropping {
+		d.draw(screen)
+	}
+	for _, b := range w.bullets {
+		b.draw(screen)
+	}
+	w.base.draw(screen)
+	load := fmt.Sprintf("Load:%d", w.base.load)
+	text.Draw(screen, load, smallArcadeFont, w.width-(len(load)*smallFontSize), smallFontSize, color.White)
 }
 
 const (
-	screenWidth  = 320
+	screenWidth  = 240
 	screenHeight = 240
 )
 
 var (
-	world  = NewWorld(screenWidth, screenHeight)
-	pixels = make([]byte, screenWidth*screenHeight*4)
+	world = NewWorld(screenWidth, screenHeight)
 )
 
 func update(screen *ebiten.Image) error {
-	world.Update()
+	world.logicupdate()
 
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
 
-	world.Draw(pixels)
-	screen.ReplacePixels(pixels)
+	world.Draw(screen)
 	return nil
 }
 
 func main() {
+	tt, err := truetype.Parse(fonts.ArcadeN_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	const dpi = 72
+
+	smallArcadeFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    smallFontSize,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+
 	if err := ebiten.Run(update, screenWidth, screenHeight, 2, "Faller Experiment"); err != nil {
 		log.Fatal(err)
 	}
